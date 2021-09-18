12c3f1f968a90aca2c7cafc3eacd3fbc4ac3dd67
pongzers <you@example.com>
2021-09-18 14:12:11 +0800

    removing base overrwrites

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import BaseRouter from './routes';
 // Import css to remove margin
 import './index.css';
 import './index.scss';
+import './styles/layout.scss';
 
 const app = (
 	<Router>
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import RecyclingManVector from '../../Assets/VectorMan.png';
 import Button from '../../components/Button';
+import styles from './index.module.scss';
 
 const HomePage = () => {
 	return (
-		<>
-			<h1> GUNI {<br/>} GUNI</h1>
-			<img src={RecyclingManVector} alt="vector"/>
-			<h1> Modern Recycling for Modern Families </h1>
+		<div className={`${styles.homePage} page-container`}>
+			<img className={styles.heroVector} src={RecyclingManVector} alt="vector"/>
+			<h1 className={styles.heroTitle}> GUNI {<br/>} GUNI</h1>
+			<p className={styles.heroDesc}> Modern Recycling for Modern Families </p>
 			<Button 
-				color="green"
-				type="secondary"
-				size="small"
+				color="white"
+				type="primary"
+				size="medium"
 				title="Let's Start"
+				className={styles.button}
 				onClick={() => null}
 			/>
-		</>
+		</div>
 	);
 };
 
