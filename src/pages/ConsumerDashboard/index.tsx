38dc8ace53ad9ae1ad2df59d5f0a748e17a94e5c
Lau Siaw Sam <lausiawsam98@gmail.com>
2021-09-18 17:10:57 +0800

    Add ConsumerDashboard page

--- src/pages/ConsumerDashboard/index.tsx
+++ src/pages/ConsumerDashboard/index.tsx
@@ -2,8 +2,77 @@ import React from 'react';
 import styles from './index.module.scss';
 import Nav from '../../components/Nav';
 import Button from '../../components/Button';
+import ExpandableCard from '../../components/ExpandableCard';
+
+export enum RequestStatus {
+	Accepted = 'Accepted',
+	NotAccepted = 'Not Accepted'
+}
+
+export interface IRequest {
+	date: string;
+	time: string;
+	status: RequestStatus;
+	address: string;
+}
 
 const ConsumerDashboard = () => {
+	const dummyData: IRequest[] = [
+		{
+			date: '21st sept 2021',
+			time: '2pm',
+			status: RequestStatus.Accepted,
+			address: '123 bedok rd Block 123123 Singapore 123742'
+		},
+		{
+			date: '21st sept 2021',
+			time: '2pm',
+			status: RequestStatus.NotAccepted,
+			address: '123 bedok rd'
+		},
+		{
+			date: '21st sept 2021',
+			time: '2pm',
+			status: RequestStatus.NotAccepted,
+			address: '123 bedok rd'
+		},
+		{
+			date: '21st sept 2021',
+			time: '2pm',
+			status: RequestStatus.NotAccepted,
+			address: '123 bedok rd'
+		}
+	];
+
+	const renderCard = ({ date, time, status, address }: IRequest) => {
+		const isAccepted = status === RequestStatus.Accepted;
+		const header = (
+			<div className={styles.cardHeader}>
+				<p>
+					{date}, {time}
+				</p>
+				<p className={isAccepted ? styles.accepted : styles.notAccepted}>
+					{status}
+				</p>
+			</div>
+		);
+
+		const body = (
+			<div className={styles.cardBody}>
+				<p>Location: {address}</p>
+				<Button
+					color="white" // dummy value, it'll be overwritten to be red in the styles
+					type="primary"
+					size="medium"
+					title="Delete"
+					className={styles.deleteButton}
+				/>
+			</div>
+		);
+
+		return <ExpandableCard header={header} body={body} />;
+	};
+
 	return (
 		<div className={`${styles.consumerDashboard} page-container`}>
 			<Nav />
@@ -16,6 +85,7 @@ const ConsumerDashboard = () => {
 					title="Create a new request"
 					className={styles.addButton}
 				/>
+				{dummyData.map(renderCard)}
 			</div>
 		</div>
 	);
