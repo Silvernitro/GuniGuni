f39af2ef184e3ab46d8a21a0c481be4daecb952e
pongzers <you@example.com>
2021-09-18 15:04:38 +0800

    Remove package

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -7,18 +7,20 @@ import {
 
 /* Styles */
 import styles from './index.module.scss';
+import NavIcon from '../../Assets/nav.svg';
 
 const Nav = () => {
     const [visible, setVisible] = React.useState(false)
     const options: any[] = [
         {title: 'Home', to: '/', icon: 'home'},
-        {title: 'Login', to: '/', icon: 'home'},
-        {title: 'Explore', to: '/', icon: ''},
-        {title: 'Requests', to: '/', icon: ''},
+        {title: 'Login', to: '/', icon: 'user'},
+        {title: 'Explore', to: '/', icon: 'search'},
+        {title: 'Requests', to: '/', icon: 'tags'},
     ]
 		return (
         <>
-            <button onClick={() => setVisible(true)}> hi </button>
+            <button onClick={() => setVisible(true)} className={styles.navButton}> 
+                <img src={NavIcon} className={styles.navIcon} alt="icon"/> </button>
             <Sidebar
                 className={styles.nav}
               animation='overlay'
